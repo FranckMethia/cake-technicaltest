4f419273172084d63cf59fdba8d9cad97ebc8a5c
Vincent Lamanna <v@cakemail.com>
2012-09-19 17:12:41 -0300

    Adding some comments.

--- Clients.class.php
+++ Clients.class.php
@@ -3,26 +3,31 @@
 class Clients
 {
 	// GET /clients
+	// Returns the list of clients in JSON
 	public function doList()
 	{
 	}
 
 	// GET /clients/{clientId}
+	// Returns the client's information in JSON
 	public function doGet($clientId)
 	{
 	}
 
 	// PUT /clients/{clientId}
+	// Returns the id of the client in JSON
 	public function doSet($clientId)
 	{
 	}
 
 	// POST /clients/
+	// Returns the id of the client in JSON
 	public function doCreate()
 	{
 	}
 
 	// DELETE /clients/{clientId}
+	// Returns the id of the client in JSON
 	public function doDelete($clientId)
 	{
 	}
