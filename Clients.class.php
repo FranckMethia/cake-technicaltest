86721a42a82d2858356612796275711b53f1697d
root <root@localhost.(none)>
2012-09-19 19:32:07 +0000

    Adding empty test.

---
+++ Clients.class.php
@@ -0,0 +1,29 @@
+<?php
+
+class Clients
+{
+	// GET /clients
+	public function doList()
+	{
+	}
+
+	// GET /clients/{clientId}
+	public function doGet($clientId)
+	{
+	}
+
+	// PUT /clients/{clientId}
+	public function doSet($clientId)
+	{
+	}
+
+	// POST /clients/
+	public function doCreate()
+	{
+	}
+
+	// DELETE /clients/{clientId}
+	public function doDelete($clientId)
+	{
+	}
+}
